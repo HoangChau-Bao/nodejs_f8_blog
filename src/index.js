87e101c5175b9326063a8bf94aa2bf464254ba06
Hoang <baohoang040200@gmail.com>
2021-04-08 23:10:17 +0700

    Hoang doi cau truc file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const morgan = require('morgan')
+const handlebars = require('express-handlebars');
 const app = express()
 const port = 3000
 
+//Http logger
 app.use(morgan('combined'))
 
-app.get('/tin-tuc', (req, res) => {
-    res.send('Hello World!')
+//template engine
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+
+app.get('/trang-chu', (req, res) => {
+    res.render('home')
 })
 
 app.listen(port, () => {
