994ea0b5f83b8dbd0ccc8369b2839fd84c3a2c12
Hoang <baohoang040200@gmail.com>
2021-04-10 13:19:16 +0700

    Hoang add search.hbs va METHOD parameter

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars');
+const { execPath } = require('process');
 const app = express()
 const port = 3000
 
 //static
 app.use(express.static(path.join(__dirname, 'public')));
 
+//middleware 
+app.use(express.urlencoded({
+  extended: true //body parser được tích học từ express 4.16
+}));
+app.use(express.json());
+
+
 //Http logger
-app.use(morgan('combined'))
+//app.use(morgan('combined'))
 
 //template engine
 app.engine('hbs', handlebars({
@@ -20,13 +28,26 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 //console.log(path.join(__dirname, 'resources/views'));
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     res.render('home')
-})
+});
 
 app.get('/news', (req, res) => {
+  //console.log(req.query.q);
   res.render('news')
-})
+});
+
+
+app.get('/search', (req, res) => {
+  res.render('search')
+});
+
+app.post('/search', (req, res) => {
+
+  console.log(req.body);
+
+  res.send('')
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
