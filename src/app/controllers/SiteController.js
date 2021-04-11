0586b0f1090c4c7f5715bf23d8e3b15d82f107ab
baohoang040200 <baohoang040200@gmail.com>
2021-04-11 18:15:25 +0700

    READDB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,15 +1,15 @@
 const Course = require('../models/Couse');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
   //[GET] /#
   index(req, res, next) {
-    // Course.find({}, function (err, courses) {
-    //   if (!err) res.json(courses);
-    //   else res.status(400).json({ error: 'ERROR!!!' });
-    // });
-
-    Course.find({})
-      .then((courses) => res.render('home', { courses: courses }))
+    Course.find({}) //promise
+      .then((courses) => {
+        res.render('home', {
+          courses: mutipleMongooseToObject(courses),
+        });
+      })
       .catch(next);
   }
 
