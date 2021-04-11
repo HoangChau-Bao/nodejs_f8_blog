c8b21c3551af8eb8ab62cf14f693507ae692c67a
Hoang <baohoang040200@gmail.com>
2021-04-11 14:56:40 +0700

    test husky

--- src/routes/site.js
+++ src/routes/site.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const siteController = require('../app/controllers/SiteController');
 
-        router.use('/search', siteController.search);
-        router.use('/contact', siteController.contact);
-        router.use('/', siteController.index);
+router.use('/search', siteController.search);
+router.use('/contact', siteController.contact);
+router.use('/', siteController.index);
 
 module.exports = router;
